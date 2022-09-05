1db060409b018ba8d2dc6f8995b2d366a9306f6a
shakhawat589 <shakhawat589@gmail.com>
2022-09-05 21:52:24 +0600

    Sidenav design

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/main.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 8080,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: () => [require("autoprefixer")],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        loader: "file-loader",
+        options: {
+          outputPath: "fonts",
+        },
+      },
+    ],
+  },
+};
